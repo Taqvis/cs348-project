28dc51c668421d9858cf81fbfcd7a92750a5759f
DhvanilkumarPatel <neil-develop@pm.me>
2023-05-25 19:16:15 -0400

    Added basic template for backend. File/package names temporary.

---
+++ cs348-project-backend/src/main/java/com/example/cs348project/service/MusicService.java
@@ -0,0 +1,12 @@
+package com.example.cs348project.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MusicService {
+
+    public String helloWorld() {
+        return "Hello World!";
+    }
+
+}
