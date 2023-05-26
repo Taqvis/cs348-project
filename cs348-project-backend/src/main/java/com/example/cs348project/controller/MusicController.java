276e673593e6ae2f54642a680d9668c8e0ee4e07
DhvanilkumarPatel <neil-develop@pm.me>
2023-05-25 21:45:48 -0400

    Added MySQL connection

--- cs348-project-backend/src/main/java/com/example/cs348project/controller/MusicController.java
+++ cs348-project-backend/src/main/java/com/example/cs348project/controller/MusicController.java
@@ -3,6 +3,7 @@ package com.example.cs348project.controller;
 import com.example.cs348project.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,9 +12,14 @@ public class MusicController {
     @Autowired
     MusicService musicService;
 
-    @GetMapping(path = "/hello")
+    @GetMapping(path = "/test/hello")
     public String helloWorld() {
         return musicService.helloWorld();
+    }
+
+    @PostMapping(path = "/test/entity")
+    public String createExampleEntity() {
+        return musicService.createExampleEntity();
     }
 
 }
--- cs348-project-backend/src/main/java/com/example/cs348project/service/MusicService.java
+++ cs348-project-backend/src/main/java/com/example/cs348project/service/MusicService.java
@@ -1,12 +1,25 @@
 package com.example.cs348project.service;
 
+import com.example.cs348project.entity.ExampleEntity;
+import com.example.cs348project.repository.ExampleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class MusicService {
 
+    @Autowired
+    private ExampleRepository exampleRepository;
+
     public String helloWorld() {
         return "Hello World!";
+    }
+
+    public String createExampleEntity() {
+        ExampleEntity exampleEntity = new ExampleEntity();
+        exampleEntity.setName("Example name");
+        exampleRepository.save(exampleEntity);
+        return "Saved";
     }
 
 }
