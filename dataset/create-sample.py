f3ac6c9ce399323b59077ef1cadb8f65d019fc77
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2023-06-10 16:38:10 -0400

    Split artists

--- dataset/create-sample.py
+++ dataset/create-sample.py
@@ -8,11 +8,18 @@ random.seed("cs348")
 # read the prod dataset
 prodDf = pd.read_csv("./dataset-prod.csv", index_col=0)
 
+# split artists into lists
+prodDf = prodDf.assign(**{"artists": prodDf["artists"].str.split(';')})
+
 # generate the indexes to select
 sampleIndexes = random.sample(range(len(prodDf.index)), 100)
 
 # get the rows at the indexes
 sampleDf = prodDf.iloc[sampleIndexes]
 
+# split artists into multiple rows
+sampleArtists = sampleDf.explode("artists").rename(columns={"artists": "artist"})[["track_id", "artist"]]
+
 # output sample dataset
-sampleDf.to_csv("./dataset-sample.csv")
+sampleDf.drop(columns="artists").to_csv("./dataset-sample.csv")
+sampleArtists.to_csv("./dataset-sample-artists.csv")
