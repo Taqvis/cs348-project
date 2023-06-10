b872898ab4c9451221e6a2cf8e8bb2056aecf361
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2023-06-10 16:03:25 -0400

    Created sample dataset

---
+++ dataset/create-sample.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import random
+
+# set a seed for to reproduce our sample generation
+# change the seed to obtain a different sample
+random.seed("cs348")
+
+# read the prod dataset
+prodDf = pd.read_csv("./dataset-prod.csv", index_col=0)
+
+# generate the indexes to select
+sampleIndexes = random.sample(range(len(prodDf.index)), 10)
+
+# get the rows at the indexes
+sampleDf = prodDf.iloc[sampleIndexes]
+
+# output sample dataset
+sampleDf.to_csv("./dataset-sample.csv")
