59be2e7957481002ed433617b49eb956e58944f8
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2023-06-21 23:09:15 -0400

    Added simple api for getting tracks

--- cs348-project-backend/src/main/java/com/example/cs348project/controller/MusicController.java
+++ cs348-project-backend/src/main/java/com/example/cs348project/controller/MusicController.java
@@ -1,9 +1,13 @@
 package com.example.cs348project.controller;
 
+import com.example.cs348project.entity.TrackArtistEntity;
+import com.example.cs348project.entity.TrackEntity;
 import com.example.cs348project.service.MusicService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,4 +26,24 @@ public class MusicController {
         return musicService.createExampleEntity();
     }
 
+    @GetMapping(path="/test/tracks")
+    public @ResponseBody Iterable<TrackEntity> getAllTracks() {
+        // This returns a JSON or XML with the users
+        return musicService.getAllTracks();
+    }
+
+    @GetMapping("/test/tracks/{name}")
+    public Iterable<TrackEntity> findByName(@PathVariable final String name){
+        return musicService.findTrackByName(name);
+    }
+
+    @GetMapping("/test/tracks/artists")
+    public Iterable<TrackArtistEntity> getAllArtistsByName(){
+        return musicService.getAllArtists();
+    }
+
+    @GetMapping("/test/tracks/artists/{name}")
+    public Iterable<TrackArtistEntity> findArtistsByName(@PathVariable final String name){
+        return musicService.findArtistsByName(name);
+    }
 }
