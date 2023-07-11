228b990176097619791483842cd10568bdca4a54
DhvanilkumarPatel <neil-develop@pm.me>
2023-07-11 07:57:24 -0400

    cleanup

--- backend/src/main/java/com/example/cs348project/repository/PlaylistRepository.java
+++ backend/src/main/java/com/example/cs348project/repository/PlaylistRepository.java
@@ -5,7 +5,6 @@ import com.example.cs348project.entity.PlaylistID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
