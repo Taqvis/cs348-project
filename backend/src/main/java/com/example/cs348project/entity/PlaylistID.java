1f837994c90e2b066fe41cd2daf66a47a17691cf
deliberateEmbankment <55965022+deliberateEmbankment@users.noreply.github.com>
2023-07-26 20:00:29 -0400

    Fixed error 500 when loading leaderboard where user has multiple playlists

--- backend/src/main/java/com/example/cs348project/entity/PlaylistID.java
+++ backend/src/main/java/com/example/cs348project/entity/PlaylistID.java
@@ -1,12 +1,14 @@
 package com.example.cs348project.entity;
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class PlaylistID implements Serializable {
     private String username;
     private String playlistName;
